c9ef3c7a0c1889d6ab33bfe34ba097715531eb68
fannarl <12133432+fannarl@users.noreply.github.com>
2021-10-06 17:19:12 +0000

    camera fixed

--- Matrices.py
+++ Matrices.py
@@ -137,6 +137,7 @@ class ViewMatrix:
     def move(self, del_u, del_v, del_n):
         temp_n = self.n
         temp_n.y = 0
+        temp_n.normalize()
         self.eye += self.u * del_u + self.v * del_v + temp_n * del_n
 
     def roll(self, angle):
@@ -190,10 +191,10 @@ class ViewMatrix:
 
     def add_pitch(self, n):
         self.pitch_angle += n
-        if self.pitch_angle > pi:
-            self.pitch_angle = pi
-        if self.pitch_angle < -pi:
-            self.pitch_angle = -pi
+        if self.pitch_angle > pi/2:
+            self.pitch_angle = pi/2
+        if self.pitch_angle < -pi/2:
+            self.pitch_angle = -pi/2
 
 
 
